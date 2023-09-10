e879011d86b4a5df6801002e2716f56189e901aa
Jeff <doylejeff98@gmail.com>
2023-09-10 01:17:04 -0400

    url for api post request with info

---
+++ src/components/AnimeInfo.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "./ui/Loading";
+import "./AnimeInfo.css";
+
+function AnimeInfo() {
+  const { id } = useParams();
+
+  const [info, setInfo] = useState(null);
+
+  async function getInfo() {
+    const query = `
+      query ($id: Int) {
+        Media(id: $id, type: ANIME, isAdult: true) {
+          id
+          title {
+            english
+          }
+          coverImage {
+            extraLarge
+          }
+          popularity
+          trending
+          description
+          siteUrl
+          trailer {
+            thumbnail
+          }
+          startDate {
+            year
+            month
+            day
+          }
+        }
+      }
+    `;
+
+    
+    const variables = {
+      id: `${id}`,
+    };
+
+    
+    const url = 'https://graphql.anilist.co';
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
+    };
+
+    // API code to retrieve data and handle errors
+    function handleResponse(response) {
+      return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+      });
+    }
+
+    function handleError(error) {
+      alert("Error, check console");
+      console.error(error);
+    }
+
+    // Make the HTTP Api request
+    let data = await fetch(url, options)
+      .then(handleResponse)
+      .catch(handleError);
+    console.log(data.data.Media);
+    setInfo(data.data.Media);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getInfo();
+    }, 1000);
+    // eslint-disable-next-line
+  }, []);
+
+  return info ? (
+    <div className="container">
+      <div className="row">
+        <div className="info__container">
+          <figure className="info__img--wrapper">
+            <img
+              src={info.coverImage.extraLarge}
+              alt=""
+              className="info__img"
+            />
+          </figure>
+          <div className="info__details">
+            <h2 className="info__title">{info.title.english}</h2>
+            <div
+              className="info__description"
+              dangerouslySetInnerHTML={{ __html: info.description }}
+            ></div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+}
+
+export default AnimeInfo;
